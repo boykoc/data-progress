39f6dbb71bac586d792520a00d6351554f34f96b
Cody Boyko <codyboyko@yahoo.ca>
2017-12-13 21:30:28 +0000

    wip: Let user click through organizations with animations.
    Instead of listing out all organizations, let the user
    click through to the next organization and view each
    card of data at a time.
    
    Disable buttons when there are no more organizations
    to go to (next and previous).
    
    Add basic fade animations when the app loads (see previous commit)
    and when a new organization is selected. To do this without an abrupt
    effect on the user I had to wrap the container with another div
    that has the same background colour as the animated/faded sections.
    Without this it would flash white at first. If I put the animation
    container within the section flex container it would insert a span
    and break my flexbox container.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,23 @@ import Organizations from '../organizations'
 import { CSSTransitionGroup } from 'react-transition-group'
 
 class App extends Component {
+  state = {
+    organizations: [],
+    selectedOrganizationIndex: 0
+  }
+  
+  componentDidMount() {
+    this.setState({ organizations: Organizations.organizations })
+  }
+  
+  handleNext = () => {
+    this.setState({ selectedOrganizationIndex: this.state.selectedOrganizationIndex + 1 })
+  }
+  
+  handlePrevious = () => {
+    this.setState({ selectedOrganizationIndex: this.state.selectedOrganizationIndex - 1 })
+  }
+  
   render() {
     return (
       <CSSTransitionGroup
@@ -18,10 +35,12 @@ class App extends Component {
           <header className="container">
             <h1 className="app-title">Data Progress</h1>
           </header>
-          {Organizations.organizations.map((organization) => (
-            <ProgressItem key={organization.acronym} organization={organization} />
-          ))}
+          {this.state.organizations.length > 0 && 
+            <ProgressItem organization={this.state.organizations[this.state.selectedOrganizationIndex]} />
+          }
         </div>
+        <button onClick={this.handlePrevious} disabled={this.state.selectedOrganizationIndex <= 0}>Previous</button>
+        <button onClick={this.handleNext} disabled={this.state.selectedOrganizationIndex >= this.state.organizations.length - 1}>Next</button>
       </CSSTransitionGroup>
     );
   }
--- src/components/ProgressItem.js
+++ src/components/ProgressItem.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup' 
 
 class ProgressItem extends Component {
     render() {
         return (
+            <div className='purple'>
+            <CSSTransitionGroup
+            key={this.props.organization.acronym}
+        transitionName="app-load"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
             <section className='container'>
+            
                 <p className='title'>{this.props.organization.name}</p>
                 <div className='item'>
                     <p className='value'>{this.props.organization.releasedToDate}</p>
@@ -17,7 +27,7 @@ class ProgressItem extends Component {
                     <p className='value'>{this.props.organization.releasedAsOpen}</p>
                     <p className='value-label'>Released as Open</p>
                 </div>
-            </section>
+            </section></CSSTransitionGroup></div>
         )
     }
 }
