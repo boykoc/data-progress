1746e4a021ab38b5a828fabfb6a0fd0f042eb07b
Cody Boyko <codyboyko@yahoo.ca>
2017-12-13 20:25:28 +0000

    Install react-transition-group for component animations.
    Setup some basic animation ability and add a single animation
    for the app initially loading.

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,26 @@ import logo from '../logo.svg';
 import '../App.css';
 import ProgressItem from './ProgressItem'
 import Organizations from '../organizations'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="container">
-          <h1 className="app-title">Data Progress</h1>
-        </header>
-        {Organizations.organizations.map((organization) => (
-          <ProgressItem key={organization.acronym} organization={organization} />
-        ))}
-      </div>
+      <CSSTransitionGroup
+        transitionName="app-load"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
+        <div className="App">
+          <header className="container">
+            <h1 className="app-title">Data Progress</h1>
+          </header>
+          {Organizations.organizations.map((organization) => (
+            <ProgressItem key={organization.acronym} organization={organization} />
+          ))}
+        </div>
+      </CSSTransitionGroup>
     );
   }
 }
