dddfb1c3d0c6e55b9dbeb85d8dbd3401490c4824
Cody Boyko <codywboyko@gmail.com>
2017-12-27 13:01:58 -0500

    Remove unused logo import.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import ProgressItem from './ProgressItem'
 import Organizations from '../organizations'
