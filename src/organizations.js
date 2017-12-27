b854d140e9be8fb1308cabeeca1ab8b9a5cde62c
Cody Boyko <codywboyko@gmail.com>
2017-12-27 14:33:11 -0500

    Add data for organizations.
    Add some real data for each ministry.
    
    This data was manually calculated, the 
    data structure should be changed and the code base 
    should be updated to take the raw data available
    and calculate the proper values as needed. 
    
    These numbers may be incorrect due to human error, 
    they are also a count instead of a percentage like
    the charts are expecting.

--- src/organizations.js
+++ src/organizations.js
@@ -1,18 +1,228 @@
 const Organizations = {
     organizations: [
         {
-            name: 'Organization One',
-            acronym: 'OO',
-            releasedToDate: 86,
-            releasedCurrentYear: 5,
+            name: 'ADO',
+            acronym: 'ADO',
+            releasedToDate: 1,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 1 
+        },
+        {
+            name: 'CO',
+            acronym: 'CO',
+            releasedToDate: 0,
+            releasedCurrentYear: 0,
             releasedAsOpen: 0
         },
         {
-            name: 'Organization Two',
-            acronym: 'OT',
+            name: 'EDU',
+            acronym: 'EDU',
+            releasedToDate: 161,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 18
+        },
+        {
+            name: 'ENERGY',
+            acronym: 'ENERGY',
+            releasedToDate: 10,
+            releasedCurrentYear: 3,
+            releasedAsOpen: 5
+        },
+        {
+            name: 'MAESD',
+            acronym: 'MAESD',
+            releasedToDate: 125,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 16
+        },
+        {
+            name: 'MAG',
+            acronym: 'MAG',
+            releasedToDate: 109,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 18
+        },
+        {
+            name: 'MCI',
+            acronym: 'MCI',
             releasedToDate: 49,
-            releasedCurrentYear: 76,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 6
+        },
+        {
+            name: 'MCSCS',
+            acronym: 'MCSCS',
+            releasedToDate: 13,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 15
+        },
+        {
+            name: 'MCSS',
+            acronym: 'MCSS',
+            releasedToDate: 141,
+            releasedCurrentYear: 0,
             releasedAsOpen: 4
+        },
+        {
+            name: 'MCYS',
+            acronym: 'MCYS',
+            releasedToDate: 143,
+            releasedCurrentYear: 5,
+            releasedAsOpen: 42
+        },
+        {
+            name: 'MEDG',
+            acronym: 'MEDG',
+            releasedToDate: 83,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 11
+        },
+        {
+            name: 'MFA',
+            acronym: 'MFA',
+            releasedToDate: 5,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 1
+        },
+        {
+            name: 'MGCS',
+            acronym: 'MGCS',
+            releasedToDate: 65,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 22
+        },
+        {
+            name: 'MHO',
+            acronym: 'MHO',
+            releasedToDate: 19,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MIA',
+            acronym: 'MIA',
+            releasedToDate: 2,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MIRR',
+            acronym: 'MIRR',
+            releasedToDate: 1,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MIT',
+            acronym: 'MIT',
+            releasedToDate: 15,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MMA',
+            acronym: 'MMA',
+            releasedToDate: 67,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 26
+        },
+        {
+            name: 'MNDM',
+            acronym: 'MNDM',
+            releasedToDate: 71,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 13
+        },
+        {
+            name: 'MNRF',
+            acronym: 'MNRF',
+            releasedToDate: 221,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 171
+        },
+        {
+            name: 'MOECC',
+            acronym: 'MOECC',
+            releasedToDate: 107,
+            releasedCurrentYear: 1,
+            releasedAsOpen: 51
+        },
+        {
+            name: 'MOF',
+            acronym: 'MOF',
+            releasedToDate: 73,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 20
+        },
+        {
+            name: 'MOHLTC',
+            acronym: 'MOHLTC',
+            releasedToDate: 67,
+            releasedCurrentYear: 6,
+            releasedAsOpen: 19
+        },
+        {
+            name: 'MOI',
+            acronym: 'MOI',
+            releasedToDate: 27,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MOL',
+            acronym: 'MOL',
+            releasedToDate: 38,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 8
+        },
+        {
+            name: 'MRIS',
+            acronym: 'MRIS',
+            releasedToDate: 14,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 10
+        },
+        {
+            name: 'MSA',
+            acronym: 'MSA',
+            releasedToDate: 6,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 0
+        },
+        {
+            name: 'MSW',
+            acronym: 'MSW',
+            releasedToDate: 8,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 3
+        },
+        {
+            name: 'MTCS',
+            acronym: 'MTCS',
+            releasedToDate: 85,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 16
+        },
+        {
+            name: 'MTO',
+            acronym: 'MTO',
+            releasedToDate: 147,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 27
+        },
+        {
+            name: 'OMAFRA',
+            acronym: 'OMAFRA',
+            releasedToDate: 271,
+            releasedCurrentYear: 0,
+            releasedAsOpen: 80
+        },
+        {
+            name: 'TBS',
+            acronym: 'TBS',
+            releasedToDate: 80,
+            releasedCurrentYear: 2,
+            releasedAsOpen: 10
         }
     ]
 }
